24d599e0dab0701df2edd826607c5e56c2a31fa3
Normilando <86866114+Normilando@users.noreply.github.com>
2025-09-02 18:49:34 -0300

    Esqueleto antes do merge

--- HeimdallWeb/Controllers/HomeController.cs
+++ HeimdallWeb/Controllers/HomeController.cs
@@ -18,12 +18,17 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult User()
+    public IActionResult UserPage()
     {
         return View();
     }
 
     public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public IActionResult Form()
     {
         return View();
     }
