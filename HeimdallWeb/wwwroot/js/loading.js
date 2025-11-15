e0c5e0cdfbf1b1f9057d494862a8839c87d9a05f
Normilando <86866114+Normilando@users.noreply.github.com>
2025-11-15 11:28:07 -0300

    Design novo inicio

---
+++ HeimdallWeb/wwwroot/js/loading.js
@@ -0,0 +1,7 @@
+function loading() {
+    var static = document.getElementById("analyze");
+    var spin = document.getElementById("analyzing");
+
+    static.classList.add("d-none");
+    spin.classList.remove("d-none");
+}
